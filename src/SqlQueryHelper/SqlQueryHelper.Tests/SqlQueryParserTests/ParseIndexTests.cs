c7b45bbd22467db923b74b73a7f8b90e05e2fa02
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-07-06 16:01:24 +0700

    ParseIndex

--- src/SqlQueryHelper/SqlQueryHelper.Tests/SqlQueryParserTests/ParseIndexTests.cs
+++ src/SqlQueryHelper/SqlQueryHelper.Tests/SqlQueryParserTests/ParseIndexTests.cs
@@ -9,10 +9,30 @@ public class ParseIndexTests
         var sql = """
             CREATE NONCLUSTERED INDEX [IDX_Name]
             ON [dbo].[TableName] ([Col1],[Col2],[Col3],[Col4])
+            INCLUDE([Col5],[Col6],[Col7],[Col8],[Col9],[Col10],[Col11])
+            WITH (ONLINE = ON)
             """;
 
         // Act
         var indexInfo = SqlQueryParser.ParseIndex(sql);
+
+        Assert.Equal("[IDX_Name]", indexInfo.Name);
+        Assert.Equal("[dbo].[TableName]", indexInfo.TableName);
+    }
+
+    [Fact]
+    public void ParseIndexV2()
+    {
+        // Arrange
+        var sql = """
+            CREATE NONCLUSTERED INDEX[IDX_Name]
+            ON[dbo].[TableName]([Col1],[Col2],[Col3],[Col4])
+            INCLUDE([Col5],[Col6],[Col7],[Col8],[Col9],[Col10],[Col11])
+            WITH (ONLINE = ON)
+            """;
+
+        // Act
+        var indexInfo = SqlQueryParser.ParseIndexV2(sql);
 
         Assert.Equal("[IDX_Name]", indexInfo.Name);
         Assert.Equal("[dbo].[TableName]", indexInfo.TableName);
--- src/SqlQueryHelper/SqlQueryHelper/SqlQueryParser.cs
+++ src/SqlQueryHelper/SqlQueryHelper/SqlQueryParser.cs
@@ -1,4 +1,6 @@
-﻿namespace SqlQueryHelper;
+﻿using System.Text;
+
+namespace SqlQueryHelper;
 
 public class SqlQueryParser
 {
@@ -51,6 +53,153 @@ public class SqlQueryParser
         indexInfo.TableName = tokens[indexOfONKeyword + 1];
 
         return indexInfo;
+    }
+
+    public static IndexInfo ParseIndexV2(string sqlQuery)
+    {
+        var indexInfo = new IndexInfo();
+
+        var tokens = ParseTokens(sqlQuery).ToArray().AsSpan();
+
+        var index = 0;
+        var indexOfCREATEKeyword = -1;
+        var indexOfINDEXKeyword = -1;
+        var indexOfONKeyword = -1;
+        var indeOfIndexColumns = -1;
+        var indexOfINCLUDEKeyword = -1;
+        var indexOfWITHKeyword = -1;
+
+        foreach (var token in tokens)
+        {
+            if (token.Equals("CREATE", StringComparison.OrdinalIgnoreCase))
+            {
+                indexOfCREATEKeyword = index;
+            }
+
+            if (token.Equals("INDEX", StringComparison.OrdinalIgnoreCase))
+            {
+                indexOfINDEXKeyword = index;
+            }
+
+            if (token.Equals("ON", StringComparison.OrdinalIgnoreCase))
+            {
+                indexOfONKeyword = index;
+            }
+
+            if (token.StartsWith("(", StringComparison.OrdinalIgnoreCase) && indexOfONKeyword > -1 && indeOfIndexColumns == -1)
+            {
+                indeOfIndexColumns = index;
+            }
+
+            if (token.Equals("INCLUDE", StringComparison.OrdinalIgnoreCase))
+            {
+                indexOfINCLUDEKeyword = index;
+            }
+
+            if (token.Equals("WITH", StringComparison.OrdinalIgnoreCase))
+            {
+                indexOfWITHKeyword = index;
+            }
+
+            index++;
+        }
+
+        indexInfo.Name = string.Join(string.Empty, tokens.Slice(indexOfINDEXKeyword + 1, indexOfONKeyword - indexOfINDEXKeyword - 1).ToArray());
+        indexInfo.TableName = string.Join(string.Empty, tokens.Slice(indexOfONKeyword + 1, indeOfIndexColumns - indexOfONKeyword - 1).ToArray());
+
+        return indexInfo;
+    }
+
+    private static List<string> ParseTokens(string sqlQuery)
+    {
+        var tokens = new List<string>();
+        var currentToken = new StringBuilder();
+
+        var index = 0;
+        while (index < sqlQuery.Length)
+        {
+            var c = sqlQuery[index];
+
+            if (char.IsWhiteSpace(c))
+            {
+                if (currentToken.Length > 0)
+                {
+                    tokens.Add(currentToken.ToString());
+                    currentToken.Clear();
+                }
+            }
+            else if (c == '[')
+            {
+                if (currentToken.Length > 0)
+                {
+                    tokens.Add(currentToken.ToString());
+                    currentToken.Clear();
+                }
+
+                var quotedString = ParseQuotedString(sqlQuery, '[', ']', index, ref index);
+                tokens.Add(quotedString);
+            }
+            else if (c == '(')
+            {
+                if (currentToken.Length > 0)
+                {
+                    tokens.Add(currentToken.ToString());
+                    currentToken.Clear();
+                }
+
+                var quotedString = ParseQuotedString(sqlQuery, '(', ')', index, ref index);
+                tokens.Add(quotedString);
+            }
+            else if (c == '.' || c == ',' || c == '=')
+            {
+                if (currentToken.Length > 0)
+                {
+                    tokens.Add(currentToken.ToString());
+                    currentToken.Clear();
+                }
+
+                tokens.Add(c.ToString());
+            }
+            else
+            {
+                currentToken.Append(c);
+            }
+
+            index++;
+        }
+
+        return tokens;
+    }
+
+    private static string ParseQuotedString(string sqlQuery, char beginQuote, char endQuote, int start, ref int index)
+    {
+        var count = 1;
+
+        index++;
+
+        while (index < sqlQuery.Length)
+        {
+            var c = sqlQuery[index];
+
+            if (c == beginQuote)
+            {
+                count++;
+            }
+            else if (c == endQuote)
+            {
+                count--;
+
+                if (count == 0)
+                {
+                    var rs = sqlQuery.Substring(start, index - start + 1);
+                    return rs;
+                }
+            }
+
+            index++;
+        }
+
+        throw new Exception("Invalid Syntax.");
     }
 }
 
