7e87a00ad0379a1f915fcedf2ac45189fe448297
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-07-08 06:38:55 +0700

    MigrationBuilderExtensions

---
+++ src/SqlQueryHelper/SqlQueryHelper.Microsoft.EntityFrameworkCore.SqlServer/MigrationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SqlQueryHelper.Microsoft.EntityFrameworkCore.SqlServer;
+
+internal static class MigrationBuilderExtensions
+{
+    public static void CreateIndexIfNotExists(this MigrationBuilder migrationBuilder, string tableName, string indexName, string script)
+    {
+        var sql = SqlQueryGenerator.CreateIndexIfNotExists(tableName, indexName, script);
+        migrationBuilder.Sql(sql);
+    }
+
+    public static void DropIndexIfExists(this MigrationBuilder migrationBuilder, string tableName, string indexName)
+    {
+        var sql = SqlQueryGenerator.DropIndexIfExists(tableName, indexName);
+        migrationBuilder.Sql(sql);
+    }
+}
