f16eb59a73ebcdc3521a5b9ffdc71e6e20d6b8f0
Phong Nguyen <nguyendoanphong.it@gmail.com>
2025-07-24 16:23:27 +0700

    SqlQueryExecutor

--- src/SqlServerQueryHelper/SqlServerQueryHelper.EntityFrameworkCore/SqlQueryExecutor.cs
+++ src/SqlServerQueryHelper/SqlServerQueryHelper.EntityFrameworkCore/SqlQueryExecutor.cs
@@ -1,39 +1,69 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 
 namespace SqlServerQueryHelper.EntityFrameworkCore;
 
 public class SqlQueryExecutor
 {
-    public static void ExecuteSql(string path, string connectionString, Action<string> log = null)
+    private static readonly bool DebugEnabled = false;
+
+    public static void ExecuteSqlFiles(string path, string connectionString, Action<string> log = null)
     {
         foreach (var file in Directory.EnumerateFiles(path, "*.sql").OrderBy(f => f))
         {
-            log?.Invoke($"Executing File: {Path.GetFileName(file)}");
-
-            string script = File.ReadAllText(file);
-
-            using var connection = new SqlConnection(connectionString);
-            connection.Open();
-
-            foreach (var sql in SqlQueryParser.SplitSqlBatches(script))
-            {
-                if (string.IsNullOrWhiteSpace(sql))
-                {
-                    continue;
-                }
-
-                log?.Invoke($"Executing Batch:");
-                log?.Invoke(sql);
-
-                using var command = new SqlCommand(sql, connection);
-                command.ExecuteNonQuery();
-            }
+            ExecuteSqlFile(file, connectionString, log);
         }
     }
 
-    public static void ExecuteSql(string path, DbContext dbContext, Action<string> log = null)
+    public static void ExecuteSqlFile(string file, string connectionString, Action<string> log = null)
     {
-        ExecuteSql(path, dbContext.Database.GetConnectionString()!, log);
+        log?.Invoke($"Executing File: {Path.GetFileName(file)}");
+
+        string script = File.ReadAllText(file);
+
+        StringBuilder test = null;
+
+        if (DebugEnabled)
+        {
+            test = new StringBuilder();
+        }
+
+        using var connection = new SqlConnection(connectionString);
+        connection.Open();
+
+        foreach (var sql in SqlQueryParser.SplitSqlBatches(script))
+        {
+            if (string.IsNullOrWhiteSpace(sql))
+            {
+                continue;
+            }
+
+            if (DebugEnabled)
+            {
+                test!.AppendLine(sql);
+            }
+
+            log?.Invoke($"Executing Batch:");
+            log?.Invoke(sql);
+
+            using var command = new SqlCommand(sql, connection);
+            command.ExecuteNonQuery();
+        }
+
+        if (DebugEnabled)
+        {
+            File.WriteAllText(file + ".debug", test!.ToString());
+        }
+    }
+
+    public static void ExecuteSqlFiles(string path, DbContext dbContext, Action<string> log = null)
+    {
+        ExecuteSqlFiles(path, dbContext.Database.GetConnectionString()!, log);
+    }
+
+    public static void ExecuteSqlFile(string file, DbContext dbContext, Action<string> log = null)
+    {
+        ExecuteSqlFile(file, dbContext.Database.GetConnectionString()!, log);
     }
 }
